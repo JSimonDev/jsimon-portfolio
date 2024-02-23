895daac34014db5558bb87b235f64d877c786b66
Jonathan Simon <yuditbertoli@gmail.com>
2024-02-23 12:41:44 -0400

    Built web
    Branch: main
    Commit: 6d3a61c24fd247eb716a74673d569fccf598f8e9 (dirty)
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,8 +31,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "d3d72ad81ca63d6a4ac9cceaab75bc8c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c3f06dd1d4666384cbed145a60514b79",
-"/": "c3f06dd1d4666384cbed145a60514b79",
+"index.html": "00454f5f616595687a6b21267768f182",
+"/": "00454f5f616595687a6b21267768f182",
 "main.dart.js": "ae8a78f991c2fa83f8328f5b4171b7a5",
 "manifest.json": "d8fe34f7ae4c072a77b924e01dac8a50",
 "version.json": "9b818ca9511483c901bed1545384376c"};
