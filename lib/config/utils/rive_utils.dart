eb512bf60015679ebe0125b29b15c97628dfbbb4
Jonathan Simon <yuditbertoli@gmail.com>
2024-03-09 19:19:38 -0400

    Actualizar método getRiveController en RiveUtils

--- lib/config/utils/rive_utils.dart
+++ lib/config/utils/rive_utils.dart
@@ -1,8 +1,10 @@
 import 'package:rive/rive.dart';
 
 class RiveUtils {
-  static StateMachineController getRiveController(Artboard artboard,
-      {stateMachineName = 'State Machine 1'}) {
+  static StateMachineController getRiveController(
+    Artboard artboard, {
+    stateMachineName = 'State Machine 1',
+  }) {
     StateMachineController? controller =
         StateMachineController.fromArtboard(artboard, stateMachineName);
     artboard.addController(controller!);
